9d9c1686ab9422799969185a4d7c35c14a3172fa
ivam_ <ivam@tut.by>
2023-02-10 17:37:33 +0300

    init

---
+++ src/main/java/by/ivam/kameleoonTrialTask/KameleoonTrialTaskApplication.java
@@ -0,0 +1,13 @@
+package by.ivam.kameleoonTrialTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonTrialTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KameleoonTrialTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ivam/kameleoonTrialTask/KameleoonTrialTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ivam.kameleoonTrialTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTrialTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
